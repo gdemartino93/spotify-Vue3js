1b8629f5d8c7eea418a8ed04e9104a082e775da1
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-29 16:47:26 +0100

    top 20 tracks array

---
+++ src/components/Song.vue
@@ -0,0 +1,44 @@
+<script >
+export default {
+props :["item"],
+data(){
+    return{
+        
+    }
+},
+methods:{
+    // topSongs(){
+    //     let topSongs = store.topSongs;
+    // }
+}
+}
+</script>
+
+<template>
+<div class="song">
+    <div class="cover">
+            <!-- <img :src="item.share.image" alt=""> -->
+    </div>
+    <div class="description">
+        <span class="title">titolp</span>
+        <span class="author">autore</span>
+    </div>
+
+</div>
+</template>
+
+<style lang="scss">
+.song{
+    width: calc(100% / 6 - 20px);
+    height: 200px;
+    background:green;
+    border: 1px solid red;
+
+    img{
+        width: 100%;
+        height: 100%;
+    }
+}
+
+
+</style>
